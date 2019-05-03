8ab3d6125f890953cae436fc923ffc6383759848
reeson2003 <reeson2003@gmail.com>
2019-05-03 18:46:02 +0300

    task 5 new methods

--- task5/src/main/java/OrderedTree.java
+++ task5/src/main/java/OrderedTree.java
@@ -1,48 +1,55 @@
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
-public class OrderedTree<Key extends Comparable<Key>, Value> extends RBTree<CompoundKey<Key>, Value> implements Iterable<Value> {
+public class OrderedTree<Key extends Comparable<Key>, Value>
+        extends RBTree<CompoundKey<Key>, Value>
+        implements Iterable<OrderedTree.Pair<CompoundKey<Key>, Value>> {
 
     public void putOne(Key key, Value value) {
         CompoundKey<Key> compoundKey = new CompoundKey<>(key);
-        compoundKey.setNumber(size);
+        compoundKey.setNumber(size());
         put(compoundKey, value);
     }
 
     public void delete(int index) {
-        Node<CompoundKey<Key>, Value> node = getNode(index);
-        delete(node.getKey());
-        int number = node.getKey().getNumber();
+        Node node = getNode(index);
+        int number = node.key.getNumber();
         preOrderTraversal(root, n -> {
-            int num = n.getKey().getNumber();
+            int num = n.key.getNumber();
             if (num > number)
-                n.getKey().setNumber(num - 1);
+                n.key.setNumber(num - 1);
         });
+        delete(node.key);
     }
 
-    protected Node<CompoundKey<Key>, Value> getNode(int index) {
-        if (index > size)
+    protected Node getNode(int index) {
+        if (index > size())
             return null;
-        AtomicReference<Node<CompoundKey<Key>, Value>> result = new AtomicReference<>();
+        AtomicReference<Node> result = new AtomicReference<>();
         preOrderTraversal(root, node -> {
-            if (node.getKey().getNumber() == index)
+            if (node.key.getNumber() == index)
                 result.set(node);
         });
         return result.get();
     }
 
-    public Value get(int index) {
+    public Pair<CompoundKey<Key>, Value> get(int index) {
         return Optional.ofNullable(getNode(index))
-                .map(Node::getValue)
+                .map(node -> new Pair<>(node.key, node.val))
                 .orElse(null);
     }
 
     @Override
-    public Iterator<Value> iterator() {
-        return new Iterator<Value>() {
-            private int remains = size;
+    public Iterator<Pair<CompoundKey<Key>, Value>> iterator() {
+        return new Iterator<Pair<CompoundKey<Key>, Value>>() {
+            private int remains = size();
 
             @Override
             public boolean hasNext() {
@@ -50,11 +57,64 @@ public class OrderedTree<Key extends Comparable<Key>, Value> extends RBTree<Comp
             }
 
             @Override
-            public Value next() {
+            public Pair<CompoundKey<Key>, Value> next() {
                 if (!hasNext())
                     throw new NoSuchElementException();
-                return get(size - remains--);
+                return get(size() - remains--);
             }
         };
+    }
+
+    protected void preOrderTraversal(Node n, Consumer<Node> handler) {
+        handler.accept(n);
+        if (n.left != null)
+            preOrderTraversal(n.left, handler);
+        if (n.right != null)
+            preOrderTraversal(n.right, handler);
+    }
+
+    public void erase(int from, int to) {
+        for (int i = 0; i < to - from; i++) {
+            delete(from);
+        }
+    }
+
+    public void exclude(OrderedTree<Key, Value> another) {
+        another.forEach(pair -> {
+            AtomicInteger toDelete = new AtomicInteger(-1);
+            preOrderTraversal(root, node -> {
+                if (pair.key.getKey().equals(node.key.getKey()))
+                    toDelete.set(node.key.getNumber());
+            });
+            int index = toDelete.get();
+            if (index >= 0) {
+                delete(index);
+            }
+        });
+    }
+
+    public void multiple(int times) {
+        if (times <= 1)
+            return;
+        List<Pair<Key, Value>> pairs = StreamSupport.stream(spliterator(), false)
+                .map(pair -> new Pair<>(pair.key.getKey(), pair.value))
+                .collect(Collectors.toList());
+        for (int i = 0; i < times - 1; i++) {
+            pairs.forEach(pair -> putOne(pair.key, pair.value));
+        }
+    }
+
+    public void print() {
+        preOrderTraversal(root, System.out::println);
+    }
+
+    public static final class Pair<K, V> {
+        public final K key;
+        public final V value;
+
+        private Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
--- task5/src/test/java/OrderedTreeTest.java
+++ task5/src/test/java/OrderedTreeTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class OrderedTreeTest {
 
@@ -12,7 +12,8 @@ public class OrderedTreeTest {
         tree.putOne(2, 2);
         tree.putOne(2, 3);
         tree.putOne(3, 3);
-        assertEquals(5, tree.size);
+        assertEquals(5, tree.size());
+        tree.print();
     }
 
     @Test
@@ -23,10 +24,49 @@ public class OrderedTreeTest {
         tree.putOne(2, 2);
         tree.putOne(2, 3);
         tree.putOne(3, 3);
-        tree.preOrderTraversal(tree.root, System.out::println);
-        System.out.println();
         tree.delete(3);
-        tree.preOrderTraversal(tree.root, System.out::println);
+        assertEquals(4, tree.size());
+        tree.print();
+    }
 
+    @Test
+    public void erase() {
+        OrderedTree<Integer, Integer> tree = new OrderedTree<>();
+        tree.putOne(1, 1);
+        tree.putOne(1, 2);
+        tree.putOne(2, 2);
+        tree.putOne(2, 3);
+        tree.putOne(3, 3);
+        tree.erase(1, 3);
+        assertEquals(2, tree.size());
+        tree.print();
+    }
+
+    @Test
+    public void exclude() {
+        OrderedTree<Integer, Integer> tree = new OrderedTree<>();
+        tree.putOne(1, 1);
+        tree.putOne(1, 2);
+        tree.putOne(2, 2);
+        tree.putOne(2, 3);
+        tree.putOne(3, 3);
+        OrderedTree<Integer, Integer> subtree = new OrderedTree<>();
+        subtree.putOne(2, 2);
+        subtree.putOne(2, 3);
+        subtree.putOne(3, 3);
+        tree.exclude(subtree);
+        assertEquals(2, tree.size());
+        tree.print();
+    }
+
+    @Test
+    public void multiple() {
+        OrderedTree<Integer, Integer> tree = new OrderedTree<>();
+        tree.putOne(1, 1);
+        tree.putOne(2, 2);
+        tree.putOne(3, 3);
+        tree.multiple(3);
+        assertEquals(9, tree.size());
+        tree.print();
     }
 }

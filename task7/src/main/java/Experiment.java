e7a7a67fa454d8189d1bddfe385c3907e420f131
reeson2003 <reeson2003@gmail.com>
2019-05-25 15:31:55 +0300

    task 7 export to excel add

--- task7/src/main/java/Experiment.java
+++ task7/src/main/java/Experiment.java
@@ -1,7 +1,9 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -15,6 +17,7 @@ public class Experiment implements Runnable {
     public static final String IN_FILE = "in.txt";
     public static final String OUT_FILE = "out.txt";
     public static final String EXCEL_FILE = "EXAMPLE.xls";
+    public static final String EXCEL_COPY_FILE = "COPY.xls";
     private static final OutputStream STUB = new OutputStream() {
         @Override
         public void write(int b) {
@@ -38,13 +41,51 @@ public class Experiment implements Runnable {
         }
     }
 
-    private void exportToExcel() {
-
+    private void exportToExcel() throws IOException {
+        HSSFWorkbook book = new HSSFWorkbook(new FileInputStream(EXCEL_FILE));
+        HSSFSheet sheet = book.getSheet("Лист1");
+        exportInData(sheet);
+        exportOutData(sheet);
+        book.write(new File(EXCEL_COPY_FILE));
+        book.close();
     }
 
-    private void processResults() throws IOException {
-        Process process = Runtime.getRuntime().exec(RG_32_17_EXE);
-        Files.copy(Paths.get(Experiment.OUT_FILE), process.getOutputStream());
+    private void exportOutData(HSSFSheet sheet) throws IOException {
+        List<String> list = getStrings(OUT_FILE);
+        for (int i = 0; i < list.size(); i++) {
+            HSSFRow row = sheet.getRow(i + 3);
+            String[] numbers = list.get(i).split(" ");
+            for (int j = 0; j < numbers.length; j++) {
+                row.getCell(52 + j).setCellValue(numbers[j]);
+            }
+        }
+    }
+
+    private void exportInData(HSSFSheet sheet) throws IOException {
+        List<String> list = getStrings(IN_FILE);
+        HSSFRow row = sheet.getRow(1);
+        int total = Integer.parseInt(list.get(0));
+        row.getCell(0).setCellValue(total);
+
+        for (int i = 0; i < total; i++) {
+            HSSFRow nextRow = sheet.getRow(i + 3);
+            String[] entry = list.get(i + 1).split(" ");
+            nextRow.getCell(0).setCellValue(entry[0]);
+            nextRow.getCell(1).setCellValue(entry[1]);
+        }
+    }
+
+    private List<String> getStrings(String fileName) throws IOException {
+        return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+    }
+
+    private void processResults() {
+        try {
+            Process process = Runtime.getRuntime().exec(RG_32_17_EXE);
+            Files.copy(Paths.get(Experiment.OUT_FILE), process.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void generateData() {
